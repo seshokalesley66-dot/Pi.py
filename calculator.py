3cb5be3b51adf04738703085caf06a2b79e36629
seshokalesley66-dot <seshokalesley66@gmail.com>
2026-02-21 13:44:00 +0200

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,12 @@
+a = float(input("First number: "))
+b = float(input("Second number: "))
+op = input("Operation (+,-,*,/): ")
+
+if op == "+":
+    print(a + b)
+elif op == "-":
+    print(a - b)
+elif op == "*":
+    print(a * b)
+elif op == "/":
+    print(a / b)
